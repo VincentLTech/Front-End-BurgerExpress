25714d831d81aa721ef3367092a969d20e14f8f7
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-25 12:07:33 -0500

    add navbar home and router in the app folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from  './components/Home'
 function App() {
   return (
-    <div className="App">
-HERLLO
-    </div>
+  <BrowserRouter>
+    <Routes>
+        <Route exact path="/" element={<Home />} />
+        
+      
+
+    </Routes>
+</BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+
+export default function NavBar(  ) {
+  
+    
+ 
+    return (
+        <div className = {"nav"}>
+           <Link to="/">
+                 <button className ={"home"} >Home </button>
+            </Link> 
+          
+            
+        </div>
+    )
+}
