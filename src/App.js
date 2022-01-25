066621e443f11449b70979a2e26d659e48e0dd99
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-25 14:23:37 -0500

    add the menu component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from  './components/Home'
+import Home from "./components/Home";
+import Menu from "./components/Menu";
 function App() {
   return (
-  <BrowserRouter>
-    <Routes>
+    <BrowserRouter>
+      <Routes>
         <Route exact path="/" element={<Home />} />
-        
-      
-
-    </Routes>
-</BrowserRouter>
+      </Routes>
+      <Routes>
+        <Route exact path="/menu" element={<Menu />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
