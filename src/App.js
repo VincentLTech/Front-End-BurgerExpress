67fbe05681615278c6b72982f08f55512b188064
Evelyn Velez <67163723+evs09@users.noreply.github.com>
2022-01-25 13:18:34 -0500

    Add Home Content & Styling
    Change banner image later

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from  './components/Home'
+import NavBar from './components/NavBar'
+
 function App() {
   return (
   <BrowserRouter>
+    <NavBar />
+
     <Routes>
         <Route exact path="/" element={<Home />} />
         
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,42 @@
-
-
+import React from 'react';
 
 export default  function Home() {
   return (
-  <div>Home</div>
+    <div>
+        <div id="banner-div">
+            <img src="https://www.nestle.com/sites/default/files/plant-based-bacon-cheeseburger-feed.jpg" id="banner-img" />
+            
+            <button id="banner-btn">Order Now</button>
+        </div>
+
+        <div id="press">
+            <p id="pr-text">
+                Our family business has served the community for decades striving to prepare and serve the best dishes in the neighborhood. 
+                With fresh ingredients, secret recipes, and excellent cooks, Burger Express has received many awards and great reviews from renowned critics.
+            </p>
+
+        </div>
+
+        <div id="loc-div">
+            <table id="loc-tbl">
+                <tr>
+                    <td id="loc-img-td">
+                        <img src="https://www.creativefabrica.com/wp-content/uploads/2020/05/01/Fast-food-restaurant-building-vector-Graphics-4002365-1-1-580x386.jpg" id="loc-img" />
+                    </td>
+                    <td>
+                        <p> 123 Main St.<br />
+                            Brooklyn, NY <br />
+                            11210 <br />
+                            (555) 123 - 4321
+                        </p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
+
+    </div>
   );
 }
 
