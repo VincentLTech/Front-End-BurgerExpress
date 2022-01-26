2d8fca313d38ef33c960f35edf43df2fa31ea572
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-26 13:51:22 -0500

    connect backend to frontend and  fetch then  render data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,76 @@
-import React from 'react';
-import {Link} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
 
 const btnStyle = {
-    position: "absolute",
-    top: "80%",
-    left: "50%",
-    transform: "translate(-50%, 0%)",
-    padding: "10px",
-    textDecoration: "none",
-    backgroundColor: "#3f0400",
-    color: "bisque",
-    fontSize: "1.5rem"
-}
+  position: "absolute",
+  top: "80%",
+  left: "50%",
+  transform: "translate(-50%, 0%)",
+  padding: "10px",
+  textDecoration: "none",
+  backgroundColor: "#3f0400",
+  color: "bisque",
+  fontSize: "1.5rem",
+};
 
-export default  function Home() {
+export default function Home() {
+  //   const [menuItems, setMenuItems] = useState([]);
+
+  //   useEffect(() => {
+  //     async function fectchData() {
+  //       const menuItemsAPi = await Axios.get(
+  //         `http://localhost:8080/api/menuItem/`
+  //       );
+  //       console.log("this", menuItemsAPi);
+  //       setMenuItems(menuItemsAPi.data);
+  //     }
+  //     fectchData();
+  //   }, []);
   return (
     <div>
-        <div id="banner-div">
-            <img src="https://www.nestle.com/sites/default/files/plant-based-bacon-cheeseburger-feed.jpg" id="banner-img" />
-            
-            <Link to="/Menu" >
-                <button style={btnStyle}>Order Now</button>
-            </Link>
-        </div>
+      <div id="banner-div">
+        <img
+          src="https://www.nestle.com/sites/default/files/plant-based-bacon-cheeseburger-feed.jpg"
+          id="banner-img"
+        />
 
-        <div id="press">
-            <p id="pr-text">
-                Our family business has served the community for decades striving to prepare and serve the best dishes in the neighborhood. 
-                With fresh ingredients, secret recipes, and excellent cooks, Burger Express has received many awards and great reviews from renowned critics.
-            </p>
+        <Link to="/Menu">
+          <button style={btnStyle}>Order Now</button>
+        </Link>
+      </div>
 
-        </div>
+      <div id="press">
+        <p id="pr-text">
+          Our family business has served the community for decades striving to
+          prepare and serve the best dishes in the neighborhood. With fresh
+          ingredients, secret recipes, and excellent cooks, Burger Express has
+          received many awards and great reviews from renowned critics.
+        </p>
+      </div>
 
-        <div id="loc-div">
-            <table id="loc-tbl">
-                <tr>
-                    <td id="loc-img-td">
-                        <img src="https://www.creativefabrica.com/wp-content/uploads/2020/05/01/Fast-food-restaurant-building-vector-Graphics-4002365-1-1-580x386.jpg" id="loc-img" />
-                    </td>
-                    <td>
-                        <p> 123 Main St.<br />
-                            Brooklyn, NY <br />
-                            11210 <br />
-                            (555) 123 - 4321
-                        </p>
-                    </td>
-                </tr>
-            </table>
-        </div>
+      <div id="loc-div">
+        <table id="loc-tbl">
+          <tr>
+            <td id="loc-img-td">
+              <img
+                src="https://www.creativefabrica.com/wp-content/uploads/2020/05/01/Fast-food-restaurant-building-vector-Graphics-4002365-1-1-580x386.jpg"
+                id="loc-img"
+              />
+            </td>
+            <td>
+              <p>
+                {" "}
+                123 Main St.
+                <br />
+                Brooklyn, NY <br />
+                11210 <br />
+                (555) 123 - 4321
+              </p>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MiniDishCard from "./MiniDishCard";
-
+import Axios from "axios";
 const cardStyle = {
-    display: "inline-block"
-}
+  display: "inline-block",
+};
 
-export default function Menu(){
-    return (
-        <div>
-            <h2>APPETIZERS</h2>
-                {/* Insert mapping here when db is complete, pass values to dish card */}
-                <MiniDishCard />
-                <MiniDishCard />
+export default function Menu() {
+  const [menuItems, setMenuItems] = useState([]);
+  const [holder, setMenuHolder] = useState({});
 
-            <h2>ENTREES</h2>
+  useEffect(() => {
+    async function fectchData() {
+      const menuItemsAPi = await Axios.get(`http://localhost:8080/api/menu/`);
+      console.log("this", menuItemsAPi);
+      menuItemsAPi.data.sort(function (a, b) {
+        return a.type - b.type;
+      });
+      setMenuItems(menuItemsAPi.data);
+    }
+    fectchData();
 
-            <h2>DESSERT</h2>
+    console.log("this is holder" + holder);
+    console.log(holder);
+  }, []);
+  //   function structureCreater() {
+  //     let temp = { entree: [], appetizer: [], dessert: [], drink: [] };
+  //     // eslint-disable-next-line no-undef
+  //     menuItems.forEach((i) => {
+  //       if (i.type === "entree") {
+  //         temp["entree"].push(i);
+  //       } else if (i.type === "appetizer") {
+  //         temp["appetizer"].push(i);
+  //       } else if (i.type === "dessert") {
+  //         temp["dessert"].push(i);
+  //       } else if (i.type === "drink") {
+  //         temp["drink"].push(i);
+  //       }
+  //     });
+  //     console.log(temp);
+  //     setMenuHolder(temp);
+  //   }
 
-            <h2>DRINKS</h2>
+  //   console.log("this", menuItems);
+  //   Object.assign({}, ...menuItems.map((x) => ({ [x.type]: x })));
+  return (
+    <div>
+      {true && console.log(menuItems[0])}
+      {menuItems.map((item) => {
+        return (
+          <>
+            <h2> {item.type}</h2>
+            {console.log("hi")}
+            <MiniDishCard item={item} />
+          </>
+        );
+      })}
 
-        </div>
-    )
+      <h2>ENTREES</h2>
+
+      <h2>DESSERT</h2>
+
+      <h2>DRINKS</h2>
+    </div>
+  );
 }
