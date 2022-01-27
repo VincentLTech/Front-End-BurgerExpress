b1913d53198a396a0ee98125026d23773f33b060
Aseel Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2022-01-27 13:19:27 -0500

    handle login and register in the forntend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,55 @@
-import React from "react";
-import "../Login.css"; 
-import grillBg from './images/grill-bg.jpeg';
+import React, { useState } from "react";
+import Axios from "axios";
+
+import "../Login.css";
+
+import grillBg from "./images/grill-bg.jpeg";
 export default function Login() {
-    return (
-        <div>
-            <img src={grillBg} id="login-bg-img"/>
-            <div id="login-body">
-                <form className="form">
-                    <div>
-                        <label>Email: </label>
-                        <input type="text" name="Email"></input>
-                    </div>
-                    <div>
-                        <label>Password: </label>
-                        <input type="text" name="Password"></input>
-                    </div>
-                    <div>
-                        <input className="submitBtn" type="submit" value="Submit"></input>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log("response");
+  async function handleSubmit(e, values) {
+    e.preventDefault();
+    await Axios.post(`http://localhost:8080/api/customer/login`, {
+      email: "alshohateeaseel@gmail.com",
+      password: "1234",
+    }).then((response) => {
+      console.log(response);
+      localStorage.setItem("user", JSON.stringify(response.data));
+      //   if (response.data.accessToken) {
+      //     localStorage.setItem("user", JSON.stringify(response.data));
+      //   }
+
+      return response.data;
+    });
+  }
+
+  return (
+    <div>
+      <img src={grillBg} id="login-bg-img" />
+      <div id="login-body">
+        <form className="form" onSubmit={handleSubmit}>
+          <div>
+            <label>Email: </label>
+            <input
+              type="text"
+              name="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password: </label>
+            <input
+              type="text"
+              name="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <input className="submitBtn" type="submit" value="Submit"></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,41 +1,92 @@
-import React from "react";
-import "../Register.css"; 
-import grillBg from './images/NYPizzaPie.jpg';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
+import "../Register.css";
+import grillBg from "./images/NYPizzaPie.jpg";
 export default function Register() {
-    return (
-        <div>
-            <img src={grillBg} id="register-bg-img"/>
-            <div id="register-body">
-        <form className="form">
-            <div>
-                <label>First Name: </label>
-                <input type="text" name="first"></input>
-            </div>
-            <div>
-                <label>Last Name: </label>
-                <input type="text" name="last"></input>
-            </div>
-            <div>
-                <label>Phone: </label>
-                <input type="text" name="phone"></input>
-            </div>
-            <div>
-                <label>Email: </label>
-                <input type="text" name="Email"></input>
-            </div>
-            <div>
-                <label>Password: </label>
-                <input type="text" name="last"></input>
-            </div>
-            <div>
-                <label>Confirm Password: </label>
-                <input type="text" name="email"></input>
-            </div>
-            <div>
-                <input className="submitBtn" type="submit" value="Submit"></input>
-            </div>
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConf, setPasswordConf] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function handleSubmit(e, values) {
+    e.preventDefault();
+    console.log("wow");
+    if (passwordConf === password) {
+      await Axios.post(`http://localhost:8080/api/customer/register`, {
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        password,
+      });
+      setRedirect(true);
+    } else {
+      console.log("the password did not match ");
+    }
+  }
+
+  return (
+    <div>
+      <img src={grillBg} id="register-bg-img" />
+      <div id="register-body">
+        <form className="form" onSubmit={handleSubmit}>
+          <div>
+            <label>First Name: </label>
+            <input
+              type="text"
+              name="first"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Last Name: </label>
+            <input
+              type="text"
+              name="last"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Phone: </label>
+            <input
+              type="text"
+              name="phone"
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Email: </label>
+            <input
+              type="email"
+              name="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password: </label>
+            <input
+              type="password"
+              name="last"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Confirm Password: </label>
+            <input
+              type="text"
+              name="email"
+              onChange={(e) => setPasswordConf(e.target.value)}
+            />
+          </div>
+          <div>
+            <input className="submitBtn" type="submit" value="Submit" />
+          </div>
         </form>
-        </div>
+      </div>
     </div>
-    )
+  );
 }
